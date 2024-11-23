c61cda3e86f748be5122a71df092af0d224c4120
Muhammad waseem <waseemrajpoot3033@gmail.com>
2024-11-24 00:28:41 +0500

    user model updated

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -16,8 +17,8 @@ class UserSeeder extends Seeder
         User::create([
             'name' => 'Admin User',
             'email' => 'covernewsupate@gmail.com',
-            'password' => bcrypt('Pass3132@'),
-            // 'role' => 'admin',
+            'password' => Hash::make('Pass3132@'),
+            'role' => 'admin',
             'email_verified_at' => now(),
         ]);
 
