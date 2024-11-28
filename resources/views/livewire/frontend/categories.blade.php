3ed3f7349df7f5796cce103b6fc08a09db6a14c7
Muhammad waseem <waseemrajpoot3033@gmail.com>
2024-11-28 12:50:04 +0500

    ads coments

--- resources/views/livewire/frontend/categories.blade.php
+++ resources/views/livewire/frontend/categories.blade.php
@@ -232,10 +232,10 @@
                     <!-- Newsletter End -->
 
                     <!-- Ads Start -->
-                    <div class="mb-3 pb-3">
+                    {{-- <div class="mb-3 pb-3">
                         <a href=""><img class="img-fluid"
                                 src="{{ asset('frontend/img/news-500x280-4.jpg') }}" alt=""></a>
-                    </div>
+                    </div> --}}
                     <!-- Ads End -->
 
                     <!-- Popular News Start -->
